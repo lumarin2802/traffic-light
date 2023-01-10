d31b2721ea34ff2de0101a5091f4d7232ff8b4d8
lumarin2802 <marinperezluciana@gmail.com>
2023-01-10 13:14:48 +0000

    acondicionando componentes, centrando el semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,28 +1,11 @@
 import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import TrafficLight from "./semaforo.jsx";
 
 //create your first component
 const Home = () => {
-	const [selectedColor, setSelectedColor] = useState("red");
-	// setSelectedColor("blue")
 	return (
-		<div className="traffic-light">
-			
-			<div 
-			onClick={() => setSelectedColor ("red")}
-			className={"light red" + ((selectedColor === "red")? " glow" : "")}></div>
-
-			<div 
-			onClick={() => setSelectedColor ("yellow")}
-			className={"light yellow" + ((selectedColor === "yellow")? " glow" : "")}></div>
-
-			<div 
-			onClick={() => setSelectedColor ("green")}
-			className={"light green" + ((selectedColor === "green")? " glow" : "")}></div>
-			
-		</div>
+	<TrafficLight/>	
 	);
 };
 
